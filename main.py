01123680ba2203287bc093a876cebffac26186bf
Guilherme <lima.guilhermerj@gmail.com>
2025-04-13 18:57:57 -0300

    Implentado classe transacao, saque, deposito e historico

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from datetime import datetime
 
 class Conta:
     def __init__(self, cliente, numero):
@@ -75,7 +76,7 @@ class ContaCorrente(Conta):
         self._limite_saques = 3
 
     def sacar(self, valor):
-        numero_saques = len([transacao for transacao in self.historico.transacoes if transcao["tipo" == Saque.__name__]])
+        numero_saques = len([transacao for transacao in self.historico.transacoes if transacao["tipo" == Saque.__name__]])
 
         excedeu_limite = valor > self._limite
         excedeu_saques = numero_saques >= self._limite_saques
@@ -126,16 +127,60 @@ class PessoaFisica(Cliente):
 
 # Interface
 class Transacao(ABC):
-    pass
+    @abstractmethod
+    def valor(self):
+        pass
+
+    @abstractmethod
+    def registrar(self, conta):
+        pass
+
+
 
 class Deposito(Transacao):
-    pass
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.depositar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
 
 class Saque(Transacao):
-    pass
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self, conta):
+        sucesso_transacao = conta.sacar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)
 
 class Historico:
-    pass
+    def __init__(self):
+        self._transacoes = []
+
+    @property
+    def transacoes(self):
+        return self._transacoes
+
+    def adicionar_transacao(self, transacao):
+        self._transacoes.append(
+            {
+                "tipo": transacao.__class__.__name__,
+                "valor": transacao.valor,
+                "data": datetime.now().strftime("%d-%m-%Y %H:%M:%s"),
+            }
+        )
 
 
 
